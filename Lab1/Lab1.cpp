873247021190712339e6714350822ac32dad91a2
RoseMcc04 <mccormacka@appstate.edu>
2024-11-10 23:39:59 -0500

    finished func5()

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cmath>
+#include <limits>
+#include <array>
 #include "Lab1.hpp"
 
 void Lab1::func1(std::string s, int ct)
@@ -44,7 +46,34 @@ int Lab1::func4(int arr[], int len)
 
 int Lab1::func5(int arr[], int val, int len)
 {
-    return 0;
+    int diff;
+    int closest_i;
+    int minDiff = std::numeric_limits<int>::max();
+    if (len == 0) 
+    {
+        return -1;
+    }
+    if (len == 1) 
+    {
+        return 0;
+    }
+    for (int i = 0; i < len; i++) 
+    {
+        diff = std::abs(val - arr[i]);
+        if (diff == 0) 
+        {
+            return i;
+        }
+        else 
+        {
+            if (diff < minDiff) 
+            {
+                minDiff = diff;
+                closest_i = i;
+            }
+        }
+    }
+    return closest_i;
 }
 
 void Lab1::func6(int arr[], int ind, int len)
